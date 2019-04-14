e3cb40ca8ed16467e073ebae3963934a0f977298
gfrank <greglarious@gmail.com>
2019-04-14 10:12:43 -0600

    refactor moving pixel group

---
+++ MovingPixelGroup.h
@@ -0,0 +1,66 @@
+class MovingPixelGroup: public SegmentPixelGroup {
+public:
+	MovingPixelGroup(int start, int end, int* startList, int startListLength,
+			int* endList, int endListLength) :
+			SegmentPixelGroup(start, end), startList(startList), startListLength(
+					startListLength), endList(endList), endListLength(
+					endListLength) {
+	}
+
+	virtual void patternFinished() {
+		if (moveToTarget(&start, startList[curStartTarget], 1)) {
+			incrementWithWrap(&curStartTarget, startListLength);
+		}
+
+		if (moveToTarget(&end, endList[curEndTarget], 1)) {
+			incrementWithWrap(&curEndTarget, endListLength);
+		}
+
+		/*
+		Serial.print(" start:");
+		Serial.print(start);
+		Serial.print(" end:");
+		Serial.print(end);
+		Serial.print(" curStartTarget:");
+		Serial.print(curStartTarget);
+		Serial.print(" curStartTargetVal:");
+		Serial.print(startList[curStartTarget]);
+		Serial.print(" curEndTarget:");
+		Serial.print(curEndTarget);
+		Serial.print(" curEndTargetVal:");
+		Serial.println(endList[curEndTarget]);
+		*/
+
+	}
+
+protected:
+	int* startList;
+	int startListLength;
+	int curStartTarget = 0;
+
+	int* endList;
+	int endListLength;
+	int curEndTarget = 0;
+
+	void incrementWithWrap(int* target, int max) {
+		(*target)++;
+		if (*target >= max) {
+			(*target) = 0;
+		}
+	}
+
+	bool moveToTarget(int* curValue, int targetValue, int increment) {
+		bool atTarget = true;
+
+		if (*curValue < targetValue - increment) {
+			*curValue += increment;
+			atTarget = false;
+		} else if (*curValue > targetValue + increment) {
+			*curValue -= increment;
+			atTarget = false;
+		} else {
+			*curValue = targetValue;
+		}
+		return atTarget;
+	}
+};
--- examples/wipeRight/wipeRight.ino
+++ examples/wipeRight/wipeRight.ino
@@ -1,15 +1,20 @@
 #include <PatternSequencer.h>
 #include <FadePattern.h>
+#include <MovingPixelGroup.h>
+
 int8_t pins[8] = { PIN_SERIAL1_RX, PIN_SERIAL1_TX, 11, 13, 5, SDA, A4, A3 };
 Adafruit_NeoPXL8 strip(93, pins, NEO_BRG);
 PatternSequencer runner(&strip);
 
+
 void setup() {
-  FadePattern* pattern = new FadePattern(new DynamicSegmentPixelGroup(0, 1, 1, 1, 93, 93), 0,0,0, 0,0,255, 200, 0, 0, false);
-  runner.addPattern(pattern);
   strip.begin();  
   delay(2000);
   Serial.begin(115200);
+
+  MovingPixelGroup* moveGrp = new MovingPixelGroup(0, 1, new int[2]{0,20}, 2, new int[2]{1,21}, 2);
+  FadePattern* pattern = new FadePattern(moveGrp, 0,0,0, 0,0,255, 300, 0, 0, false);
+  runner.addPattern(pattern);
 }
 
 void loop() {
